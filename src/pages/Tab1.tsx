dcbc622d96ece60759e742f69c872c9c8bc774c5
leanfj <leanfj@hotmail.com>
2020-02-03 06:57:49 -0300

    Initial commit

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,68 +1,201 @@
 import {
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
   IonContent,
   IonHeader,
-  IonIcon,
-  IonItem,
-  IonLabel,
-  IonList,
-  IonListHeader,
   IonPage,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  IonButton,
+  IonIcon,
+  IonCard,
+  IonRow,
+  IonCol,
+  IonList,
+  IonLabel,
+  IonItem,
+  IonFab,
+  IonFabButton,
+  IonAlert,
+  IonToast
 } from '@ionic/react';
-import { book, build, colorFill, grid } from 'ionicons/icons';
-import React from 'react';
+
+import { trash, add, logoUsd, closeCircle } from 'ionicons/icons';
+import { useStorage } from '@ionic/react-hooks/storage';
+import React, { useState, useEffect } from 'react';
 import './Tab1.css';
 
+export interface Item {
+  itemName: string;
+  itemWeigth: string;
+  itemPrice: string;
+  itemValue: number;
+}
+
 const Tab1: React.FC = () => {
+  const [showCadNewItem, setShowCadNewItem] = useState(false);
+  const [itemList, setItemList] = useState<Item[]>([]);
+  const [totalValue, settotalValue] = useState(0);
+  const [showToast, setShowToast] = useState(false);
+  const [showToastError, setShowToastError] = useState(false);
+
+  const { get, set, remove } = useStorage();
+
+  useEffect(() => {
+    const loadSaved = async () => {
+      const listaString = await get('lista');
+      const lista = listaString ? JSON.parse(listaString) : [];
+
+      setItemList(lista);
+      //TO-DO Somar os valores totais de uma lista ja carregada
+    };
+    loadSaved();
+  }, [get]);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab One</IonTitle>
+          <IonTitle>Lista Mercado</IonTitle>
         </IonToolbar>
+        <IonFab vertical="top" horizontal="end" slot="fixed">
+          <IonFabButton onClick={() => setShowCadNewItem(true)}>
+            <IonIcon icon={add} />
+          </IonFabButton>
+        </IonFab>
       </IonHeader>
       <IonContent>
-        <IonCard className="welcome-card">
-          <img src="/assets/shapes.svg" alt="" />
-          <IonCardHeader>
-            <IonCardSubtitle>Get Started</IonCardSubtitle>
-            <IonCardTitle>Welcome to Ionic</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>
-            <p>
-              Now that your app has been created, you'll want to start building out features and
-              components. Check out some of the resources below for next steps.
-            </p>
-          </IonCardContent>
-        </IonCard>
+        <IonAlert
+          isOpen={showCadNewItem}
+          onDidDismiss={() => setShowCadNewItem(false)}
+          header={'Novo Item'}
+          inputs={[
+            {
+              name: 'itemName',
+              type: 'text',
+              placeholder: 'Nome do Item'
+            },
+            {
+              name: 'itemWeigth',
+              type: 'number',
+              placeholder: 'Peso ou Unidade'
+            },
+            {
+              name: 'itemPrice',
+              type: 'number',
+              placeholder: 'Valor do Item'
+            }
+          ]}
+          buttons={[
+            {
+              text: 'Cancelar',
+              role: 'cancel',
+              handler: () => {
+                setShowCadNewItem(false);
+              }
+            },
+            {
+              text: 'Ok',
+              handler: async e => {
+                console.log(e);
+                if (e.itemName && e.itemWeigth && e.itemPrice) {
+                  const itemVal = Number(e.itemWeigth) * Number(e.itemPrice);
+                  const itemValue = Number(itemVal.toFixed(2));
+                  const newItems = [{ ...e, itemValue }, ...itemList];
+                  setItemList(newItems);
 
-        <IonList lines="none">
-          <IonListHeader>
-            <IonLabel>Resources</IonLabel>
-          </IonListHeader>
-          <IonItem href="https://ionicframework.com/docs/" target="_blank">
-            <IonIcon slot="start" color="medium" icon={book} />
-            <IonLabel>Ionic Documentation</IonLabel>
-          </IonItem>
-          <IonItem href="https://ionicframework.com/docs/building/scaffolding" target="_blank">
-            <IonIcon slot="start" color="medium" icon={build} />
-            <IonLabel>Scaffold Out Your App</IonLabel>
-          </IonItem>
-          <IonItem href="https://ionicframework.com/docs/layout/structure" target="_blank">
-            <IonIcon slot="start" color="medium" icon={grid} />
-            <IonLabel>Change Your App Layout</IonLabel>
-          </IonItem>
-          <IonItem href="https://ionicframework.com/docs/theming/basics" target="_blank">
-            <IonIcon slot="start" color="medium" icon={colorFill} />
-            <IonLabel>Theme Your App</IonLabel>
-          </IonItem>
-        </IonList>
+                  settotalValue(totalValue + itemValue);
+
+                  setShowToast(true);
+
+                  set('lista', JSON.stringify(newItems));
+
+                  return;
+                }
+                setShowToastError(true);
+              }
+            }
+          ]}
+        />
+        <IonCard>
+          <IonList>
+            {itemList.map((item, index) => (
+              <IonItem key={`${index}`}>
+                <IonLabel>
+                  <IonRow className="ion-justify-content-between ion-align-items-center">
+                    <h2>{item.itemName}</h2>
+                    <IonButton
+                      color="danger"
+                      size="small"
+                      onClick={async () => {
+                        const listaString = await get('lista');
+                        const lista = listaString
+                          ? JSON.parse(listaString)
+                          : [];
+                        const itemToRemove = lista.splice(index, 1);
+                        set('lista', JSON.stringify(lista));
+                        setItemList(lista);
+                        settotalValue(totalValue - itemToRemove[0].itemValue);
+                      }}
+                    >
+                      <IonIcon icon={closeCircle} />
+                    </IonButton>
+                  </IonRow>
+                  <IonRow className="ion-justify-content-around itemDetails">
+                    <span>Quant./Kg - </span>
+                    {item.itemWeigth}
+                    <span>V. Un. - </span>
+                    <strong>R$</strong>
+                    {item.itemPrice}
+                    <span>V. Tot. - </span>
+                    <strong>R$</strong> {item.itemValue}
+                  </IonRow>
+                </IonLabel>
+              </IonItem>
+            ))}
+          </IonList>
+          <IonRow>
+            <IonCol>
+              <IonButton
+                color="danger"
+                expand="block"
+                onClick={() => {
+                  setItemList([]);
+                  settotalValue(0);
+                  remove('lista');
+                }}
+              >
+                <IonIcon slot="end" icon={trash} />
+                Limpar Lista
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonCard>
+        <IonRow>
+          <IonCol>
+            <IonButton
+              color="primary"
+              expand="block"
+              onClick={() => {
+                set(`lista${new Date().getTime()}`, JSON.stringify(itemList));
+              }}
+            >
+              <IonIcon slot="start" icon={logoUsd} />
+              {totalValue.toFixed(2)}
+            </IonButton>
+          </IonCol>
+        </IonRow>
+        <IonToast
+          isOpen={showToast}
+          onDidDismiss={() => setShowToast(false)}
+          message="Item incluído com sucesso"
+          duration={200}
+        />
+        <IonToast
+          isOpen={showToastError}
+          onDidDismiss={() => setShowToastError(false)}
+          message="Informe todos os dados"
+          duration={500}
+          color="danger"
+        />
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState, useEffect } from 'react';
+import {
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react';
+import { useStorage } from '@ionic/react-hooks/storage';
 
 const Tab2: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab Two</IonTitle>
+          <IonTitle>Listas Salvas</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonList>
-          <IonItem routerLink="/tab2/details">
+          <IonItem routerLink="/">
             <IonLabel>
-              <h2>Go to detail</h2>
+              <h2>Lista 01</h2>
+              <span>01/01/2020</span>
             </IonLabel>
           </IonItem>
         </IonList>
